05082accf5cc38e7b08e50dc9f2df317ba49e891
Grimtin10 <grimtin10@gmail.com>
2017-12-13 21:46:29 -0700

    Added things
    Added fire
    Gave fire timer
    Added timer

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var grid;
+var gridTimes;
 var size;
 var chance;
 var delay;
@@ -25,22 +26,13 @@ function setup() {
   defaultDelay = 0;
   delay = defaultDelay;
   grid = create2DArray(floor(width/size),floor(height/size),0);
+  gridTimes = create2DArray(grid.length,grid[0].length,0);
   for(var x=0;x<grid.length;x++){
     for(var y=0;y<grid[0].length;y++){
       if(random(0,1)*100<chance){
         grid[x][y]=1;
       }
     }
-  }
-}
-
-function mouseDragged(){
-  if(delay<=0){
-    grid[round(map(mouseX,0,width,0,grid.length,true))][round(map(mouseY,0,height,0,grid[0].length,true))] = type;
-    console.log("Debug info: " + delay,round(map(mouseX,0,width,0,grid.length-1,true)),round(map(mouseY,0,height,0,grid[0].length-1,true)), grid[round(map(mouseX,0,width,0,grid.length,true))][round(map(mouseY,0,height,0,grid[0].length,true))]);
-    delay=defaultDelay;
-  } else {
-    delay--;
   }
 }
 
@@ -69,7 +61,7 @@ function draw() {
         fill(220,220,245);
         rect(x*size,y*size,size,size);
       }
-      if(grid[x][y]==5){
+      if(grid[x][y]==5||grid[x][y]==6){
         fill(255+random(-20,20),127+random(-20,20),80+random(-20,20));
         rect(x*size,y*size,size,size);
       }
@@ -78,6 +70,22 @@ function draw() {
 }
 
 function update(){
+  if(mouseIsPressed){
+    var mX = round(map(mouseX,0,width,0,grid.length,true));
+    var mY = round(map(mouseY,0,height,0,grid[0].length,true));
+    if(delay<=0){
+      if(type==4){
+        gridTimes[mX][mY] = 75;
+      } else if(type==6){
+        gridTimes[mX][mY] = 10;
+      }
+      grid[mX][mY] = type;
+      console.log("Debug info: " + delay,round(map(mouseX,0,width,0,grid.length-1,true)),round(map(mouseY,0,height,0,grid[0].length-1,true)), grid[round(map(mouseX,0,width,0,grid.length,true))][round(map(mouseY,0,height,0,grid[0].length,true))]);
+      delay=defaultDelay;
+    } else {
+      delay--;
+    }
+  }
   if(keyIsDown(48)||keyIsDown(96)){
     type=0;
   } else if(keyIsDown(49)||keyIsDown(97)){
@@ -90,6 +98,8 @@ function update(){
     type=4;
   } else if(keyIsDown(53)||keyIsDown(101)){
     type=5;
+  } else if(keyIsDown(54)||keyIsDown(102)){
+    type=6;
   }
   for(var x=0;x<grid.length;x++){
     for(var y=0;y<grid[0].length;y++){
@@ -201,8 +211,10 @@ function updateParticle(x,y){
       if(random(0,1)<0.5){
         if(x+1<grid.length){
           if(grid[x+1][y]==0){
-            grid[x][y]=0;
             grid[x+1][y]=4;
+            gridTimes[x+1][y]=gridTimes[x][y];
+            grid[x][y]=0;
+            gridTimes[x][y]=0;
           }
         }
       } else {
@@ -242,6 +254,48 @@ function updateParticle(x,y){
           if(grid[x-1][y]==0){
             grid[x][y]=0;
             grid[x-1][y]=5;
+          }
+        }
+      }
+    }
+  }
+  if(grid[x][y]==6){
+    var temp = gridTimes[x][y];
+    var gone = false;
+    if(temp<1){
+      gone = true;
+      grid[x][y]=0;
+    }
+    if(grid[x][y-1]==0||grid[x][y-1]==2){
+      if(grid[x][y-1]==0){
+        grid[x][y]=0;
+        gridTimes[x][y-1]=temp-1;
+      } else if(grid[x][y-1]==2){
+        grid[x][y]==4;
+        gridTimes[x][y-1]=temp-1;
+      }
+      if(grid[x][y]!=4&&!gone){
+        grid[x][y-1]=6;
+      }
+    } else if(!gone){
+      if(random(0,1)<0.5){
+        if(x+1<grid.length){
+          if(grid[x+1][y]==0){
+            grid[x+1][y]=6;
+            gridTimes[x+1][y]=gridTimes[x][y];
+            grid[x][y]=0;
+            gridTimes[x][y]=0;
+            gridTimes[x+1][y]--;
+          }
+        }
+      } else {
+        if(x-1>-1){
+          if(grid[x-1][y]==0){
+            grid[x-1][y]=6;
+            gridTimes[x-1][y]=gridTimes[x][y];
+            grid[x][y]=0;
+            gridTimes[x][y]=0;
+            gridTimes[x-1][y]--;
           }
         }
       }
